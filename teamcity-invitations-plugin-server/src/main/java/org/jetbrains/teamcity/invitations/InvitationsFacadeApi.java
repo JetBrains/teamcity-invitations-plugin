e67e790786b3b3c4c9e0b36d0fb7f64ca6682e4e
Dmitry Treskunov <dmitry.treskunov@gmail.com>
2016-12-06 15:12:46 +0300

    Add a method returning list of join project invitations for the specified project

--- teamcity-invitations-plugin-server/src/main/java/org/jetbrains/teamcity/invitations/InvitationsFacadeApi.java
+++ teamcity-invitations-plugin-server/src/main/java/org/jetbrains/teamcity/invitations/InvitationsFacadeApi.java
@@ -1,8 +1,13 @@
 package org.jetbrains.teamcity.invitations;
 
+import jetbrains.buildServer.serverSide.SProject;
 import jetbrains.buildServer.users.SUser;
 import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class InvitationsFacadeApi {
 
@@ -18,5 +23,13 @@ public class InvitationsFacadeApi {
         String token = StringUtil.generateUniqueHash();
         JoinProjectInvitationType.InvitationImpl created = joinProjectInvitationType.createNewInvitation(inviter, name, token, projectExtId, roleId, multiuser);
         return invitationsStorage.addInvitation(token, created);
+    }
+
+    @NotNull
+    public List<Invitation> getJoinProjectInvitations(@NotNull SProject project) {
+        return invitationsStorage.getInvitations().stream()
+                .filter(invitation -> invitation.getType().equals(joinProjectInvitationType))
+                .filter(invitation -> project.equals(((JoinProjectInvitationType.InvitationImpl) invitation).getProject()))
+                .collect(toList());
     }
 }
